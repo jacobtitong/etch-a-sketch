5ff1cfcb4772bfccc9c8dbe5698c908139878104
Jacob Titong <titongjacob@gmail.com>
2026-05-31 22:37:49 +0800

    Make boxes of the grid appear as if there are rows and columns using flexbox

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let gridSize = 2;
+let gridSize = 4;
+let containerSize = gridSize * 960;
 
 const container = document.querySelector('#container');
+
+container.style.width = `${containerSize}px`;
+container.style.height = `${containerSize}px`;
 
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
